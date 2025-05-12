cb9a04725ea6be7209b96e07ce80f351025f9690
Priyanshu Walia <waliapriyanshu07@gmail.com>
2025-05-12 18:05:02 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+    </main>
+  );
+};
+
+export default App;
